26bf04fdbb8777e181e2473d8e3e853a7b68adc9
Pan_Janke <jan.krol.legowski@gmail.com>
2023-04-22 14:00:39 +0200

    Mail bot added

--- Gadgets/SendingEmail.py
+++ Gadgets/SendingEmail.py
@@ -1,17 +1,16 @@
 import smtplib
-#nie dziala przez gmaila xDD
-# Set up the SMTP server
+
 smtp_server = 'smtp.gmail.com'
 port = 587
 sender_email = 'hachatonagh123@gmail.com'
-password = 'Qwerty123chuj'
-receiver_email = 'szkolka00@gmail.com'
+password = 'vlmpfaflxnvuudfp'
+receiver_email = 'jan.krol.legowski@gmail.com'
 
 # Create the message
 message = """\
 Subject: Car Alert
 
-This message is send automatically to inform you that your car is drived by BABA, which increase the risk of stluczka and rozjebane sprzeglo.
+This message is send automatically to inform you that your car is drived by , which increase the risk of stluczka and rozjebane sprzeglo.
 """
 
 # Log in to the SMTP server
