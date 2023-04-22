9f75c3fc96710bc2601284af62d2c554b4e47c3b
weibik <j-szkola@wp.pl>
2023-04-22 13:32:25 +0200

    Setting current location of a user

---
+++ Location/LocationManager.py
@@ -0,0 +1,29 @@
+from geopy.geocoders import Nominatim
+import time
+from pprint import pprint
+
+app = Nominatim(user_agent="tutorial")
+
+def get_location_by_address(address):
+    """
+    This function returns a location as raw from an address
+    will repeat until success
+    """
+    time.sleep(1)
+
+    try:
+        return app.geocode(address).raw
+    except:
+        return get_location_by_address(address)
+
+def get_address_by_location(latitude, longitude, language="en"):
+    """This function returns an address as raw from a location
+    will repeat until success"""
+    # build coordinates string to pass to reverse() function
+    coordinates = f"{latitude}, {longitude}"
+    # sleep for a second to respect Usage Policy
+    time.sleep(1)
+    try:
+        return app.reverse(coordinates, language=language).raw
+    except:
+        return get_address_by_location(latitude, longitude)
